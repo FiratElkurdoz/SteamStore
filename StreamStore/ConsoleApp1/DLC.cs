5f06ee7d1dc5d34b768a20c6f4b434543c999760
Unknown <marco.prolog@gmail.com>
2018-02-02 18:09:38 +0100

    First version of classes

---
+++ StreamStore/ConsoleApp1/DLC.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+public class DLC
+{
+    public string name;
+    public float price;
+    Game game;
+
+	public DLC(string n, float p, Game g)
+	{
+        name = n;
+        price = p;
+        game = g;
+	}
+}
---
+++ StreamStore/ConsoleApp1/Store.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+public class Store
+{
+    Showcase showcase;
+
+	public Store()
+	{
+        showcase = new Showcase();
+	}
+
+    public void AddNewGame(string name, float price, int position)
+    {
+        Game newGame = new Game(name, price);
+        showcase.AddGame(newGame, position);
+    }
+
+    public void SellGame(int position)
+    {
+        showcase.RemoveGame(position);
+    }
+}
